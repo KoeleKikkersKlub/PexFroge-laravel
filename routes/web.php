e73c4b50a87350f036de9b52b7badf5c648496d5
Fased <faseddevelops@gmail.com>
2023-09-27 13:57:12 +0200

    Debugbar & start of authentication system

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthenticationController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+Route::controller(AuthenticationController::class)->group(function()
+{
+    Route::get('/register', 'register')->name('register');
+    Route::get('/login', 'login')->name('login');
+})
+?>
