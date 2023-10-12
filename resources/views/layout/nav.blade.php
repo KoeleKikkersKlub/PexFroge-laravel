e4bd76a54f85d01a39c6d17fc29157183b2b44dc
btummers <briantummers@telfort.nl>
2023-10-12 13:21:30 +0200

    added divergent changes back into source tree

--- resources/views/layout/nav.blade.php
+++ resources/views/layout/nav.blade.php
@@ -2,23 +2,27 @@
 @vite('resources/js/nav.js')
 
 
-
 <div class="triangle-element"> </div>
 
-<div class="topnav">
-<a href="#news">News</a>
+  <div class="topnav">
+
+  <div style="background-image: url('ImageLocation');background-size: cover; height:480px; padding-top:80px;">
+  <img src="alfa-college" style="height:150px">
+
 </div>
 
-<div class="hamburger-menu">
-  <input id="menu__toggle" type="checkbox" />
-  <label class="menu__btn" for="menu__toggle">
-    <span></span>
-  </label>
+  </div>
 
-  <ul class="menu__box">
-  
-    <li><a class="menu__item" href="#">Home</a></li>
-          <li><a class="menu__item" href="#">Chat</a></li>
-          <li><a class="menu__item" href="#">Bedrijven lijst</a></li>
-  </ul>
-</div>
+  <div class="hamburger-menu">
+    <input id="menu__toggle" type="checkbox" />
+    <label class="menu__btn" for="menu__toggle">
+      <span></span>
+    </label>
+
+    <ul class="menu__box">
+    
+      <li><a class="menu__item" href="">Home</a></li>
+			<li><a class="menu__item" href="">Chat</a></li>
+			<li><a class="menu__item" href="">Bedrijven lijst</a></li>
+    </ul>
+  </div>
