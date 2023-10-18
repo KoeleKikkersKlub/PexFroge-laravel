06ea464d7c14a7c95b0e3abd539f49588375eb1e
Fased <faseddevelops@gmail.com>
2023-10-18 09:41:32 +0200

    Created docent migration & model

--- app/Models/Traject.php
+++ app/Models/Traject.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+
 
 class Traject extends Model
 {
     use HasFactory;
+    public function Student() :HasOne
+    {
+        return $this->hasOne(Student::class);
+        }
+
+    public function StageBedrijf() : HasMany
+    {
+        return $this->hasMany(Bedrijf::class);
+    }
+
+    public function Docent() : HasMany
+    {
+        return $this->hasMany(Docent::class);
+    }
 }
