670831c3844b6c0a48a5bd3b17c78d13a7041257
Fased <faseddevelops@gmail.com>
2023-11-15 12:35:20 +0100

    import fixes

--- app/Http/Controllers/ImportController.php
+++ app/Http/Controllers/ImportController.php
@@ -17,18 +17,37 @@ class ImportController extends Controller
 
     public function import(Request $request)
     {
-    $request->validate([
-        'json_file' => 'required|mimes:json',
-    ]);
-
-    $json = file_get_contents($request->file('json_file')->getRealPath());
-    $data = json_decode($json, true);
-
-    foreach ($data as $item) {
-        // Set cg_id to 1 and create the record
-        StagemarktProfiel::create(array_merge($item, ['cg_id' => 1]));
-    }
-
-    return redirect()->route('import.form')->with('success', 'Data imported successfully.');
+        $request->validate([
+            'json_file' => 'required|mimes:json|max:10240', // Validate that the file is a JSON file with a maximum size of 10MB
+        ]);
+    
+        $json = file_get_contents($request->file('json_file')->getRealPath());
+        $data = json_decode($json, true);
+    
+        if (isset($data['bedrijf_dict'])) {
+            $companies = $data['bedrijf_dict'];
+    
+            foreach ($companies as $companyId => $companyData) {
+                $profiel = $companyData['profiel'] ?? null;
+    
+                if ($profiel) {
+                    // Extract only the desired fields
+                    $filteredData = [
+                        'name' => $profiel['naam'] ?? 'Unknown', // Provide a default value if 'naam' is null or missing
+                        'kvk_naam' => $profiel['kvk_naam'] ?? 'Unknown',
+                        'kvk_nummer' => $profiel['kvk_nummer'] ?? 'Unknown',
+                        'kvk_vestigingsnummer' => $profiel['kvk_vestigingsnummer'] ?? 'Unknown',
+                        'bedrijfsindeling' => $profiel['bedrijfsindeling'] ?? 'Unknown',
+                        'bedrijfsgrootte' => $profiel['bedrijfsgrootte'] ?? 'Unknown',
+                        'capaciteit' => $profiel['capaciteit'] ?? 'Unknown',
+                    ];
+    
+                    // Set cg_id to 1 and create the record
+                    StagemarktProfiel::create(array_merge($filteredData, ['cg_id' => 1]));
+                }
+            }
+        }
+    
+        return redirect()->route('import.form')->with('success', 'Data imported successfully.');
     }
 }
--- resources/views/import/form.blade.php
+++ resources/views/import/form.blade.php
@@ -22,16 +22,31 @@
     <table border="1">
         <thead>
             <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <!-- Add more columns based on your StagemarktProfiel model fields -->
+            <th>ID</th>
+        <th>Name</th>
+        <th>KVK Naam</th>
+        <th>KVK Nummer</th>
+        <th>KVK Vestigingsnummer</th>
+        <th>Bedrijfsindeling</th>
+        <th>Bedrijfsgrootte</th>
+        <th>Capaciteit</th>
+        <th>Created At</th>
+        <th>Updated At</th>
             </tr>
         </thead>
         <tbody>
             @foreach($importedData as $item)
                 <tr>
-                    <td>{{ $item->id }}</td>
-                    <td>{{ $item->name }}</td>
+                <td>{{ $item->id }}</td>
+        <td>{{ $item->name }}</td>
+        <td>{{ $item->kvk_naam }}</td>
+        <td>{{ $item->kvk_nummer }}</td>
+        <td>{{ $item->kvk_vestigingsnummer }}</td>
+        <td>{{ $item->bedrijfsindeling }}</td>
+        <td>{{ $item->bedrijfsgrootte }}</td>
+        <td>{{ $item->capaciteit }}</td>
+        <td>{{ $item->created_at }}</td>
+        <td>{{ $item->updated_at }}</td>
                     <!-- Add more columns based on your StagemarktProfiel model fields -->
                 </tr>
             @endforeach
